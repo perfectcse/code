05469706517f4ea75715eeae97a2c5ad4bd937f4
Vishal  Mishra <123309972+perfectcse@users.noreply.github.com>
2026-03-02 22:27:41 +0530

    update

--- Q-1js/Shallo.js
+++ Q-1js/Shallo.js
@@ -14,3 +14,6 @@ let clone = shallowClone(original);
 
 console.log("Original:", original);
 console.log("Clone:", clone);
+
+//“When the function is called, the spread operator creates a new object and copies the top-level properties. However, 
+//nested objects are copied by reference, not deeply cloned.”//
